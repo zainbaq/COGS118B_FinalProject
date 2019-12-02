e0976cf4b4f9029e9ce3fcd65c35a4d71349f88f
zainbaq <sbaquar@ucsd.edu>
2019-12-02 01:54:54 -0800

    implemented KNN

--- models/knn.py
+++ models/knn.py
@@ -10,6 +10,24 @@ df = pd.read_csv(data_dir, sep=',')
 
 tr_inp, tr_labels, te_inp, te_labels = train_test_split(df)
 
-n_neighbors = int(input("n_neighbors: "))
+n_neighbors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# n_neighbors = int(input("n_neighbors: "))
 
-clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+acc_list = np.array([])
+for n in n_neighbors:
+
+    clf = KNeighborsClassifier(n_neighbors=n)
+    clf.fit(tr_inp, tr_labels)
+
+    acc = clf.score(te_inp, te_labels)
+    print("\nKNN = {0} accuracy: {1}".format(n, acc))
+
+    acc_list = np.append(acc_list, acc)
+
+print("\nBest K: {0}, with accuracy {1}".format(
+    n_neighbors[np.argmax(acc_list)],
+    np.max(acc_list)
+))
+
+
+
