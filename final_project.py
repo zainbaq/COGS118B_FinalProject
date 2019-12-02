35ba996bd835541548ed0490f727544803328d81
zainbaq <sbaquar@ucsd.edu>
2019-12-01 23:08:59 -0800

    implemented script to get final usable dataset, consisting of all 256x256 images and the expression they are

--- final_project.py
+++ final_project.py
@@ -1,6 +1,7 @@
 import numpy as np 
 import pandas as pd 
 
+################################################################
 def process_names(st):
     st = list(st)
     for i in range(len(st)):
@@ -10,8 +11,10 @@ def process_names(st):
             st.pop()
     st.pop()
     st = ''.join(st)
+    st = st.replace('.', '-')
     return st
 
+# Loading in saved data
 data_dir = 'datasets/jaffe_loaded.npy'
 names_dir = 'datasets/jaffe_img_ids.txt'
 labels_dir = 'datasets/jaffe_labels.csv'
@@ -19,15 +22,14 @@ labels_dir = 'datasets/jaffe_labels.csv'
 df = pd.DataFrame(data=np.load(data_dir))
 names = pd.read_csv(names_dir, delimiter=',', header=None).to_numpy().squeeze()
 
+# Manipulate image names to match label format
 names = [process_names(name) for name in names]
 
+# Add names column to image df
 df['names'] = names
 
 df_labels = pd.read_csv(labels_dir)
 df_labels = df_labels.drop(columns='Unnamed: 0', axis=1)
-
-print(df.head(5))
-print(df_labels)
 
 '''
 The labels are given as a table of 6 mean scores for each image.
@@ -44,8 +46,20 @@ for i, row in enumerate(df_labels.to_numpy()):
     expression = np.argmax(row)
     expressions.append(expression)
 
-df_labels['PIC'] = df_labelids
+df_labels['names'] = df_labelids
 df_labels['expression'] = expressions
-print(df_labels.expression.value_counts())
 
-print(df_labels)
+df_all = pd.merge(df, df_labels, on='names')
+
+df_final = df_all.drop(
+    columns=['names', 'HAP', 'SAD', 'SUR', 'ANG', 'DIS', 'FEA'],
+    axis=1
+)
+
+df_final.to_csv(
+    'datasets/final_data.cav',
+    sep=','
+)
+
+#################################################################
+
--- preprocess.py
+++ preprocess.py
@@ -5,33 +5,55 @@ from PIL import Image
 import os
 import csv 
 
-jaffe_dir = 'datasets/raw/jaffedbase/'
+def load_data(data_dir, labels_dir, labels_target_dir):
+    # Load in data
+    data = np.array([])
+    img_names = np.array([])
+    d_size = 0
+    for filename in os.listdir(jaffe_dir):
+        if filename.endswith(".tiff"):
+            im = Image.open(os.path.join(jaffe_dir, filename))
+            imarray = np.array(im)
+            data = np.append(data, imarray)
+            name = filename.replace('.tiff', '')
+            img_names = np.append(img_names, name)
+            d_size = d_size+1
 
-# Load in data
-data = np.array([])
-img_names = np.array([])
-d_size = 0
-for filename in os.listdir(jaffe_dir):
-    if filename.endswith(".tiff"):
-        im = Image.open(os.path.join(jaffe_dir, filename))
-        imarray = np.array(im)
-        data = np.append(data, imarray)
-        name = filename.replace('.tiff', '')
-        img_names = np.append(img_names, name)
-        d_size = d_size+1
+    label_head = ['#', 'HAP', 'SAD', 'SUR', 'ANG', 'DIS', 'FEA', 'PIC']
+    labels = pd.read_csv(
+        labels_dir, 
+        sep=' ', 
+        header=None,
+        names = label_head
+        )
+    # reshape data such that each row is a 256x256 image of a face
+    data = np.reshape(data, (d_size, -1))
 
-# save img names to separate csv
-with open('datasets/jaffe_img_ids.txt', 'w') as F:
-    wr = csv.writer(F)
-    wr.writerow(img_names)
+    return data, labels, img_names
 
-# reshape data such that each row is a 256x256 image of a face
-data = np.reshape(data, (d_size, -1))
-# Save data to avoid future reading
-np.save('datasets/jaffe_loaded.npy', data)
-np.savetxt('datasets/jaffe_img_ids.csv', data, delimiter=',')
+def save_data(data, labels, img_names):
+    # Save data to avoid future reading
+    np.save('datasets/jaffe_loaded.npy', data)
+    # Save labels to separate csv
+    labels.to_csv(labels_target_dir, sep=',')
+    # save img names to separate csv
+    with open('datasets/jaffe_img_ids.txt', 'w') as F:
+        wr = csv.writer(F)
+        wr.writerow(img_names)
 
+if __name__ == '__main__':
 
+    jaffe_dir = 'datasets/jaffe/jaffedbase/'
+    labels_dir = 'datasets/jaffe/labels.txt'
+    labels_target_dir = 'datasets/jaffe_labels.csv'
+
+    data, labels, img_names = load_data(
+        jaffe_dir,
+        labels_dir,
+        labels_target_dir
+    )
+
+    save_data(data, labels, img_names)
 
 
 
